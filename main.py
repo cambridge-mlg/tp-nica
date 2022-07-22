f5075c996006a7ae83ededa24510f71db4fedd1e
HHalva <hermanni.halva@helsinki.fi>
2022-07-22 22:00:29 +0300

    remote push

--- main.py
+++ main.py
@@ -55,9 +55,9 @@ def parse():
                         help="num. of samples from q(s|tau) in elbo")
     parser.add_argument('--num-tau-samples', type=int, default=10,
                         help="num. of samples from q(tau) in elbo")
-    parser.add_argument('--phi-learning-rate', type=float, default=3e-1,
+    parser.add_argument('--phi-learning-rate', type=float, default=3e-2,
                         help="learning rate for variational params")
-    parser.add_argument('--theta-learning-rate', type=float, default=3e-4,
+    parser.add_argument('--theta-learning-rate', type=float, default=3e-2,
                         help="learning rate for model params")
     parser.add_argument('--minib-size', type=int, default=8,
                         help="minibatch size")
