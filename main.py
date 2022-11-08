fbb7f9abcfc2f00ea050a97962fb5c9246372a0b
HHalva <hermanni.halva@helsinki.fi>
2022-11-09 01:42:30 +0200

    set repeat kernels as true in main args

--- main.py
+++ main.py
@@ -146,10 +146,12 @@ def main():
         t = gen_2d_locations(args.T)
     if args.GP:
         x, z, s, *params = gen_gpnica_data(data_key, t, args.N, args.M,
-                              args.L_data, args.num_data, mu_fn, k_fn)
+                              args.L_data, args.num_data, mu_fn, k_fn,
+                              repeat_kernels=True)
     else:
         x, z, s, tau, *params = gen_tpnica_data(data_key, t, args.N, args.M,
-                              args.L_data, args.num_data, mu_fn, k_fn)
+                              args.L_data, args.num_data, mu_fn, k_fn,
+                              repeat_kernels=True, repeat_dfs=True)
 
 
     # just to plot data for now:
