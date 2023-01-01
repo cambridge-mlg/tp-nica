conjugate gradient almost working but need to ensure that few steps are correct

inference.py
@@ -55,25 +55,27 @@ def structured_elbo_s(key, theta, phi_s, logpx, cov_fn, x, t, tau, nsamples):
     h = h.T.reshape(-1)
     L_full = vmap(fill_tril, in_axes=(1, None), out_axes=-1)(L, N)
     J = vmap(jnp.matmul, in_axes=(-1, -1))(L_full, L_full.swapaxes(0, 1))
-#    Jinv = vmap(lambda a: js.linalg.cho_solve((a, True), jnp.eye(N)),
-#                in_axes=-1)(L_full)
-#    K_Jinv = K.at[jnp.arange(T), jnp.arange(T)].add(Jinv).swapaxes(1, 2).reshape(N*T, N*T)
+    Jinv = vmap(js.linalg.cho_solve, in_axes=((-1, None), None))(
+        (L_full, True), jnp.eye(N))
+    K_Jinv = K.at[jnp.arange(T), jnp.arange(T)].add(Jinv).swapaxes(1, 2).reshape(N*T, N*T)
     K = K.swapaxes(1, 2).reshape(N*T, N*T)
-
 #    A_inv = jnp.linalg.inv(jnp.linalg.inv(K)+J)
 #    logZ = 0.5*h.T@A_inv@h + 0.5*jnp.linalg.slogdet(A_inv)[1] - \
 #        0.5*jnp.linalg.slogdet(K)[1]
 
-    # set preconditioners
-    P_k = pivoted_cholesky(K, tol=1e-9, max_rank=3)
-    PinvK = solve_precond_plus_block_diag(P_k, J, K)
-    DinvK = solve_precond_plus_diag(P_k, vmap(jnp.diag)(J).reshape(-1), K)
+    # set preconditioners and func to calculate its inverse matrix product
+    P_k = pivoted_cholesky(K, tol=1e-9, max_rank=10)
+    Pinv_fun = lambda _: solve_precond_plus_block_diag(P_k, J, _)
+    #Pinv_fun = lambda _: solve_precond_plus_diag(
+    #    P_k, vmap(jnp.diag)(J).reshape(-1), _)
 
-    #dbg
-    Jinv = jnp.linalg.inv(js.linalg.block_diag(*J))
-    PinvK2 = jnp.linalg.inv((1/vmap(jnp.diag)(J).reshape(-1)) *jnp.eye(Jinv.shape[0]) + P_k@P_k.T)@K
-    pdb.set_trace()
+    # run mbcg
+    A_fun = partial(jnp.matmul, K_Jinv)
+    out = mbcg_solve(A_fun, K@h, maxiter=20, M=Pinv_fun)
 
+
+#    Jinv2 = js.linalg.block_diag(*Jinv)
+#    test = jnp.linalg.inv(Jinv2+K)@(K@h)
     #logZ2 = 0.5*(h.T@Jinv)@jnp.linalg.inv(Jinv+K)@(K@h) - 0.5*jnp.linalg.slogdet(
     #    Jinv+K)[1] + 0.5*jnp.linalg.slogdet(Jinv)[1]
 
@@ -107,7 +109,7 @@ def structured_elbo_s(key, theta, phi_s, logpx, cov_fn, x, t, tau, nsamples):
     #KLqpu = -0.5*(tr+h.T@L@h)+WTy.T@h - logZ
     s, _ = rngcall(lambda _: jr.multivariate_normal(_, jnp.zeros((T, N)),
                 jnp.eye(N), shape=(nsamples, T)), key)
-    return jnp.zeros((0,)), s+lax.stop_gradient(P.sum())
+    return jnp.zeros((0,)), s+lax.stop_gradient(out[0].sum())+out[1][0].sum()
                       #Elogpx-KLqpu, s
 
 

utils.py
@@ -27,6 +27,11 @@ from util import tree_get_idx
 from tensorflow_probability.substrates.jax.distributions import WishartTriL
 
 
+# some lambdas
+_id_fun = lambda _: _
+_reshape_vec_2d = lambda _: jnp.reshape(_, (-1, 1))
+
+
 def sample_wishart(key, v0, W0):
     W0_chol = jnp.linalg.cholesky(W0)
     return WishartTriL(v0, scale_tril=W0_chol).sample(seed=key)
@@ -231,38 +236,41 @@ def load_checkpoint(train_args):
     return ckpt, hist
 
 
+#@partial(jit, static_argnames=['A', 'M', 'maxiter'])
 def mbcg_solve(A, B, x0=None, *, tol=1e-5, maxiter=None, M=None):
-    def cond_fun(value, tol=tol, maxiter=maxiter):
-        _, R, *_, j = value
+    def cond_fun(value):
+        *_, R, j = value
         errs = jnp.sum(R**2, 0)**0.5
-        return jnp.all(errs > tol) and (j < maxiter)
+        return jnp.all(errs > tol) & (j < maxiter)
 
 
     def body_fun(value):
-        U, triDs, R, D, Z, a, b, j = value
+        U, triDs, D, Z, a, b, R, j = value
         _V = A(D)
         _a = (R*Z).sum(0) / (D*_V).sum(0)
-        _U = U+jnp.diag(_a)@D
-        _R = R-jnp.diag(_a)@_V
+        _U = U + _a.reshape(1, -1)*D
+        _R = R - _a.reshape(1, -1)*_V
         _Z = M(_R)
         _b = (_Z*_Z).sum(0) / (Z*Z).sum(0)
-        _D = _Z-jnp.diag(_b)@D
+        _D = _Z - _b.reshape(1, -1)*D
         _triDs = [triDs[i].at[j, j].set(
-            1/_a[i] + cond(j == 0, lambda x, y: 0, lambda x, y: y/x, a[i], b[i])
+            1/_a[i] + cond(j == 0, lambda x, y: 0., jnp.divide, b[i], a[i])
         ) for i in range(len(triDs))]
         _triDs = [cond(j == 0, lambda x, y, z: z,
             lambda x, y, z: z.at[[j-1, j], [j, j-1]].set(jnp.sqrt(y)/x),
             _a[i], b[i], triDs[i]) for i in range(len(triDs))]
-        return _U, _triDs, _R, _D, _Z, _a, _b, j+1
+        _triDs = triDs
+        return _U, _triDs, _D, _Z, _a, _b, _R, j+1
 
 
+    B = B.reshape(B.shape[0], -1)
     n, t = B.shape
     U0 = jnp.zeros((n, t))
-    R0 = _sub(A(U0), B)
+    R0 = _sub(B, A(U0))
     Z0 = M(R0)
     D0 = Z0.copy()
-    triDs0 = [jnp.zeros((maxiter, maxiter)) for _ in range(t)]
-    init_val = (U0, triDs0, R0, D0, Z0, jnp.zeros(t), jnp.zeros(t), 0)
+    trids0 = [jnp.zeros((maxiter, maxiter)) for _ in range(t)]
+    init_val = (U0, trids0, D0, Z0, jnp.zeros(t), jnp.zeros(t), R0, 0)
     U_final, triDs_final, *_ = while_loop(cond_fun, body_fun, init_val)
     return U_final, triDs_final
 