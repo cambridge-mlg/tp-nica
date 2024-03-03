031bf5857acd82419dc6a738bd8e0622d627e719
HHalva <halva.hermanni@gmail.com>
2024-03-04 01:35:34 +0200

    done with cv4a data fetching and basic iVAE baseline implementation

--- cv4a_data.py
+++ cv4a_data.py
@@ -4,15 +4,15 @@ import argparse
 import matplotlib.pyplot as plt
 import datetime
 
-from sklearn.model_selection import StratifiedShuffleSplit
+from data_generation import gen_2d_locations
 
 import pdb
 
 
-# This code is adapted from the challenge-winning repo of KarimAmer:
+# !! This code is adapted from the challenge-winning repo of KarimAmer: !!
 # https://github.com/radiantearth/crop-type-detection-ICLR-2020/blob/master/solutions/KarimAmer/main.py
 
-def get_cv4a_data(data_path):
+def get_cv4a_data(data_path, experiment_id=1):
     imgs = np.load(os.path.join(data_path, 'imgs_13_ch_rad_16_medianxy.npy'))
     areas = np.load(os.path.join(data_path, 'areas.npy'))
     gts = np.load(os.path.join(data_path, 'gts.npy'))
@@ -53,8 +53,6 @@ def get_cv4a_data(data_path):
     fields_arr = fields_arr[gts > -1]
     gts = gts[gts > -1]
 
-
-
     # dates
     dates = [datetime.datetime(2019, 6, 6, 8, 10, 7),
              datetime.datetime(2019, 7, 1, 8, 10, 4),
@@ -69,4 +67,28 @@ def get_cv4a_data(data_path):
              datetime.datetime(2019, 9, 24, 8, 9, 59),
              datetime.datetime(2019, 10, 4, 8, 10),
              datetime.datetime(2019, 11, 3, 8, 10)]
-    return imgs, areas, fields_arr, field_masks, gts, dates
+
+
+    # reshape data and split into train and test
+    imgs = imgs.swapaxes(1, 2)
+    num_data, M, T_t, T_x, T_y = imgs.shape
+    t_spatial = gen_2d_locations(T_x*T_y)
+    t_spatial = (t_spatial - t_spatial.mean()) / t_spatial.std()
+    t_time = np.array([(d-dates[0]).days for d in dates])
+    t_time = (t_time - t_time.mean())/t_time.std()
+    t_time = t_time.repeat(T_x*T_y)
+    t = np.hstack((t_time[:, None], np.tile(t_spatial, (T_t, 1))))
+
+    # two different types of experiments (see paper)
+    if experiment_id == 1:
+        imgs = imgs.reshape(num_data, M, -1)
+        imgs_tr = imgs[:num_data//2]
+        imgs_te = imgs[num_data // 2:]
+        return imgs_tr, imgs_te, t
+    elif experiment_id == 2:
+        T_len = 6
+        t = t[:T_len * T_x * T_y]
+        t = (t - t.mean(0)[None, :]) / t.std(0)[None, :]
+        imgs_tr = imgs[:, :, :T_len].reshape(num_data, M, -1)
+        imgs_te = imgs[:, :, T_len:2 * T_len].reshape(num_data, M, -1)
+        return imgs_tr, imgs_te, t
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import jax
 from jax import config
 config.update("jax_enable_x64", True)
+import jax.numpy as jnp
 print(jax.devices())
+
+import os
+os.environ["MPLCONFIGDIR"] = "/proj/herhal/.cache/"
 
 import sys
 import hydra
 import pdb
 from omegaconf import DictConfig
+
+from cv4a_data import get_cv4a_data
+from ivae import train_ivae
 
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
@@ -17,9 +24,18 @@ def main(cfg: DictConfig) -> None:
     if cfg.synthetic:
         data = generate_synthetic()
     else:
-        data = load(cfg.data_dir)
+        x_tr, x_te, t = get_cv4a_data(cfg.data_dir, cfg.experiment_id)
 
-    train(data, cfg)
+    s_features, ivae_loss_hist = train_ivae(jnp.float32(x_tr),
+                                            jnp.float32(t),
+                                            cfg.ivae.N,
+                                            cfg.ivae.L_est,
+                                            cfg.ivae.num_epochs,
+                                            cfg.ivae.minib_size)
+
+    pdb.set_trace()
+
+    #train(data, cfg)
 
 
 if __name__ == "__main__":
